0c28c168507c7adeef5f36c04f1a86e3b1fe73ad
jonasbgi <jonasbgi@stud.ntnu.no>
2017-09-19 14:12:10 +0200

    git-ignore nonsense

--- src/gui/LoginController.java
+++ src/gui/LoginController.java
@@ -2,13 +2,19 @@ package gui;
 
 import javafx.application.Application;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
 
-public class LoginController{
+import javafx.event.ActionEvent;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable{
     @FXML
     Button btnLogin;
 
@@ -19,8 +25,15 @@ public class LoginController{
     TextField txtPassword;
 
     @FXML
-    void handleLogin(){
-        System.out.println("Logged in");
+    void handleLogin(ActionEvent event){
+        String username = txtUsrName.getText();
+        String password = txtPassword.getText();
+        System.out.println("Username: " + username);
+        System.out.println("Password: " + password);
     }
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
--- src/main.java
+++ src/main.java
@@ -1,10 +1,47 @@
 import connector.*;
+import gui.*;
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import org.kopitubruk.util.json.JSONParser;
+import org.kopitubruk.util.json.JSONUtil;
+import org.kopitubruk.util.json.JsonObject;
 
-public class main {
+
+public class main extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("/gui/loginUI.fxml"));
+
+        //LoginController loginController = new LoginController();
+        //fxmlLoader.setController(loginController);
+
+        Parent root = (Parent) fxmlLoader.load(/*this.getClass().getResource("/gui.loginUI.fxml")*/);
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        tcpSocketHandler s;
+        s = new tcpSocketHandler("127.0.0.1", 1337);
+        s.send("Hei på deg");
+        s.disconnect();
+        launch(args);
+
+    }
+    /*
     public static void main(String[] args){
         tcpSocketHandler s;
         s = new tcpSocketHandler("127.0.0.1", 1337);
         s.send("Hei på deg");
         s.disconnect();
-    }
+    }*/
 }
